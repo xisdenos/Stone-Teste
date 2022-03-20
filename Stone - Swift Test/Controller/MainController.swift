92d55b85b258c6b8a132e13718790e0890b0c57e
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-03-20 18:50:40 -0300

    Initial commit

--- Stone - Swift Test/Controller/MainController.swift
+++ Stone - Swift Test/Controller/MainController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +15,7 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func requestButton(_ sender: UIButton) {
+    }
 }
 
