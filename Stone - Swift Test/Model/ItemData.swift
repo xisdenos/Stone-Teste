4e128b960fc5785c72e47cf73fa4b72404f6e313
Lucas Pinto <lucaspinto.andrade@gmail.com>
2022-03-20 18:53:27 -0300

    Item model created

---
+++ Stone - Swift Test/Model/ItemData.swift
@@ -0,0 +1,16 @@
+//
+//  ItemData.swift
+//  Stone - Swift Test
+//
+//  Created by Lucas Pinto on 20/03/22.
+//
+
+import Foundation
+
+struct ItemModel {
+    
+    let title: String
+    let price: Int
+    let seller: String
+    let thumbnailHd: String
+}
